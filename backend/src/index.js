7404e4045e44066cfa0e8bb6b17ad9aad008ab4e
Yeo-Likheng <lykheng727@gmail.com>
2025-08-17 11:00:48 +0700

    fix routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
-app.use(express.json({ limit: "50mb" })); // allows you to parse the body of the request
+app.use(express.json({ limit: "50mb" })); 
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
@@ -37,7 +37,7 @@ app.use("/api/analytics", analyticsRoutes);
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
